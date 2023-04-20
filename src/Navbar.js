b15fe0143e12182aebd29ed6822185e0440c81c2
Kristin Lam <klam623@gmail.com>
2023-04-20 18:38:04 -0400

    Update navbar links and styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,35 +1,31 @@
-import React from "react";
-import App from "./App";
-import "./Navbar.css"
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import FlightAPI from "./FlightAPI";
-
+import React from 'react';
+import App from './App';
+import './Navbar.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import FlightAPI from './FlightAPI';
+import logo from './mlh-prep.png';
 
 function Navbar() {
   return (
     <Router>
       <div>
         <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/trip-planner">Trip Planner</Link>
-            </li>
-            <li>
-              <Link to="/climate-change">Climate Change</Link>
-            </li>
-            <li>
-              <Link to="/about-us">About us</Link>
-            </li>
-          </ul>
+          <div>
+            <Link to="/">
+              <img className="logo" src={logo} alt="MLH Prep Logo" />
+            </Link>
+          </div>
+          <div className="nav__right">
+            <Link to="/day-planner">Day Planner</Link>
+            <Link to="/trip-planner">Trip Planner</Link>
+            <Link to="/climate-change">Climate Change</Link>
+          </div>
         </nav>
         <Routes>
-          <Route exact path="/" element={<App/>} />
-          <Route path="/climate-change" element={<App/>} />
-          <Route path="/trip-planner" element={<FlightAPI/>} />
-          <Route path="/about-us" element={<App/>} />
+          <Route exact path="/" element={<App />} />
+          <Route path="/climate-change" element={<App />} />
+          <Route path="/trip-planner" element={<FlightAPI />} />
+          <Route path="/about-us" element={<App />} />
         </Routes>
       </div>
     </Router>
