d4f8a7d2a46c51fc920a68af4fbf6e533dd39fed
Kristin Lam <klam623@gmail.com>
2023-04-20 21:25:01 -0400

    Structure and style homepage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import logo from "./mlh-prep.png";
-import AutoCity from "./components/AutoCity";
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './App.css';
+import AutoCity from './components/AutoCity';
 import Forecast from './Forecast';
-import React  from 'react';
-import Essentials from "./components/essentials";
+import React from 'react';
+import Essentials from './components/essentials';
 
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState('Mumbai');
   const [results, setResults] = useState(null);
 
   const handleSelect = (suggestion) => {
@@ -24,7 +24,7 @@ function App() {
         .then((res) => res.json())
         .then(
           (result) => {
-            if (result["cod"] !== 200) {
+            if (result['cod'] !== 200) {
               setIsLoaded(false);
             } else {
               setIsLoaded(true);
@@ -41,28 +41,70 @@ function App() {
   if (error) {
     return <div>Error: {error.message}</div>;
   } else {
-    return <>
-      <img className="logo" src={logo} alt="MLH Prep Logo"></img>
-      <div className="container">
-        <div className="header">
-          <h2>Enter a city below <span role="img" aria-label="emoji">👇</span></h2>
-          <AutoCity onSelect={handleSelect} />
+    return (
+      <div className="weather__container">
+        {/* Column 1 */}
+        <div className="weather__col--1">
+          <div className="weather__search">
+            <p className="weather__search--prompt">Enter a city</p>
+            <AutoCity onSelect={handleSelect} />
+          </div>
+          <div className="weather__placeholder--1">Google Map component</div>
+          <div className="weather__placeholder--2">Playlist component</div>
         </div>
-        <div className="results">
+
+        {/* Column 2 */}
+        <div className="weather__col--2">
           {!isLoaded && <h2>Loading...</h2>}
-          {console.log(results)}
-          {isLoaded && results && (<>
-            <h3>{results.weather[0].main}</h3>
-            <p>Feels like {results.main.feels_like}°C</p>
-            <i><p>{results.name}, {results.sys.country}</p></i>
-            <Forecast city={city} />
-          </>)}
+          {isLoaded && results && (
+            <div>
+              <div className="results__main">
+                <h3>{results.weather[0].main}</h3>
+                <p>Feels like {results.main.feels_like}°C</p>
+                <i>
+                  <p>
+                    {results.name}, {results.sys.country}
+                  </p>
+                </i>
+              </div>
+
+              <div className="weather__essentials">
+                <Essentials today={results?.weather[0].main} />
+              </div>
+
+              <div>
+                <Forecast city={city} />
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* Column 3 */}
+        <div className="weather__col--3">
+          <div className="weather__cta weather__cta--day">
+            <p>Plan your day with fun activities!</p>
+            <Link to="/day-planner">
+              <button>Day Planner</button>
+            </Link>
+          </div>
+
+          <div className="weather__cta weather__cta--trip">
+            <p>Start planning your next trip with our flight finder!</p>
+            <Link to="/trip-planner">
+              <button>Trip Planner</button>
+            </Link>
+          </div>
+
+          <div className="weather__cta weather__cta--climate">
+            <p>Learn how climate change can affect your travels.</p>
+            <Link to="/climate-change">
+              <button>Climate Change</button>
+            </Link>
+          </div>
         </div>
       </div>
-      <Essentials today={results?.weather[0].main}/>
-    </>
+    );
   }
 }
 
 export default App;
-
